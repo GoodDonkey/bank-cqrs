40c03ba6658c58f3d6dc8845fb229f3d46fe44ac
GoodDonkey <president20500@gmail.com>
2022-07-10 13:03:40 +0900

    feat: holder command, event handler

--- command/src/main/java/command/aggregate/Holder.java
+++ command/src/main/java/command/aggregate/Holder.java
@@ -1,15 +1,37 @@
 package command.aggregate;
 
+import com.bankcqrs.events.HolderCreated;
+import command.commands.CreateHolderCommand;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
 
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+
 @Aggregate
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Holder {
     @AggregateIdentifier
     private String holderId;
-    private String accountId;
+    private String holderName;
     private String phoneNumber;
     private String address;
+    
+    @CommandHandler
+    public Holder(CreateHolderCommand command) {
+        apply(new HolderCreated(command.getHolderId(), command.getHolderName(), command.getPhoneNumber(), command.getAddress()));
+    }
+    
+    @EventSourcingHandler
+    protected void createAccount(HolderCreated event) {
+        this.holderId = event.getHolderId();
+        this.holderName = event.getHolderName();
+        this.phoneNumber = event.getPhoneNumber();
+        this.address = event.getAddress();
+    }
 }
