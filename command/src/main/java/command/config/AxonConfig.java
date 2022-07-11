27b4960338d90f7ccec038783b550be0e9b8f152
GoodDonkey <president20500@gmail.com>
2022-07-11 13:15:29 +0900

    feat: config CommandBus as SimpleCommandBus

---
+++ command/src/main/java/command/config/AxonConfig.java
@@ -0,0 +1,19 @@
+package command.config;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.SimpleCommandBus;
+import org.axonframework.common.transaction.TransactionManager;
+import org.axonframework.springboot.autoconfig.AxonAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AutoConfigureAfter(AxonAutoConfiguration.class)
+public class AxonConfig {
+    
+    @Bean
+    CommandBus commandBus(TransactionManager transactionManager) {
+        return SimpleCommandBus.builder().transactionManager(transactionManager).build();
+    }
+}
