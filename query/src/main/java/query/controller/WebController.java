9791ca645f80ca41e1bd5835710f3d88a24302bd
GoodDonkey <president20500@gmail.com>
2022-07-14 14:06:02 +0900

    feat: added html, web controller

---
+++ query/src/main/java/query/controller/WebController.java
@@ -0,0 +1,13 @@
+package query.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    
+    @GetMapping("/p2p")
+    public String p2pQueryView() {
+        return "p2p.html";
+    }
+}
